ef28c75461bd94bc2a20c549537203f2322315c6
Kevin Copo <kevincopo7@gmail.com>
2023-05-17 14:54:51 -0500

    Version 0.1

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <h1>Layout</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
